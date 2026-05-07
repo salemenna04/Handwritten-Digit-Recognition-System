aa2a9c648327266c5ab44e7e3c928908549dd579
salemenna04 <salemenna04@gmail.com>
2026-05-07 14:59:51 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -11,15 +11,19 @@ st.write("Upload your own handwritten digit image")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["png","jpg","jpeg"])
 
-def preprocess_image(image):
+def preprocess(image):
     image = ImageOps.grayscale(image)
     image = image.resize((28,28))
     image = np.array(image)
+
+    image = 255 - image
+
+    image = (image > 30) * image
+
     image = image / 255.0
     image = image.reshape(1,28,28,1)
-    return image
 
-if uploaded_file is not None:
+    return imageif uploaded_file is not None:
     img = Image.open(uploaded_file)
     st.image(img, caption="Uploaded Image", use_column_width=True)
 
